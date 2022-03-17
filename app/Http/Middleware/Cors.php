909f7a111590db3a8eafae7246012c7f37e6fa02
Shahmir Khan Jadoon <shahmirkj@gmail.com>
2022-03-17 19:41:43 +0500

    Added frontend domain to Cors middleware

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,7 +17,8 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
+            // ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Origin', 'https://royalty-uae-cms.com')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', '*');
     }
