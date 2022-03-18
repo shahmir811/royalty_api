be3ea7ce9ee50988b90ba7025d912d3f82b036bc
Shahmir Khan Jadoon <shahmirkj@gmail.com>
2022-03-18 18:36:51 +0500

    Updated Cors middleware

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,8 +18,13 @@ class Cors
     {
        return $next($request)
             // ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Origin', 'https://royalty-uae-cms.com')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', '*');
+            // ->header('Access-Control-Allow-Origin', 'https://royalty-uae-cms.com')
+            // ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+            // ->header('Access-Control-Allow-Headers', '*');
+                ->header('Access-Control-Allow-Origin', '*')
+                ->header('Access-Control-Allow-Methods', '*')
+                ->header('Access-Control-Allow-Credentials', true)
+                ->header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization')
+                ->header('Accept', 'application/json');            
     }
 }
